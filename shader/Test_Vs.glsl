b36c54535c967aa34e5e4c51ef782190441d2f58
mibloemer <mibloemer@uos.de>
2012-07-31 09:14:33 +0200

    vs

--- shader/Test_Vs.glsl
+++ shader/Test_Vs.glsl
@@ -3,12 +3,12 @@
 uniform mat4 model;
 uniform mat4 viewProj;
 
-in vec3 vs_in_pos;
+in vec2 positionMC;
 
 out vec3 fs_in_color;
 
 void main(void) {
 	fs_in_color = vec3(1,0,0);
-    gl_Position = viewProj * model * vec4(vs_in_pos, 1);
+    gl_Position = viewProj * model * vec4(positionMc,0, 1);
 
 }
