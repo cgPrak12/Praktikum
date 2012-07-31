4e5d504030cc46c76d5ffb3cf2e3d0439b035832
mibloemer <mibloemer@uos.de>
2012-07-31 09:30:47 +0200

    merge conflict

--- shader/Test_Vs.glsl
+++ shader/Test_Vs.glsl
@@ -3,21 +3,14 @@
 uniform mat4 model;
 uniform mat4 viewProj;
 
-<<<<<<< HEAD
-in vec2 positionMC;
-=======
+
 in vec2 positionMc;
->>>>>>> 94b290f0312cb0bc1cd2d37852840969820493da
 
 out vec3 fs_in_color;
 
 void main(void) {
-<<<<<<< HEAD
-	fs_in_color = vec3(1,0,0);
-    gl_Position = viewProj * model * vec4(positionMc,0, 1);
 
-=======
     gl_Position = viewProj * model * vec4(positionMc.x,0,positionMc.y, 1);
 	fs_in_color = vec3(sin(positionMc),1);
->>>>>>> 94b290f0312cb0bc1cd2d37852840969820493da
+
 }
