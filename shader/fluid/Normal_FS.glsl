3dad040e23cc757afd04279b628d953c661b82f9
Faedrivin <info@sebastian-hoeffner.de>
2012-08-01 23:50:00 +0200

    added some setUniforms, corrected(?) depth_texture to show positions and store depth in w-component

--- shader/fluid/Normal_FS.glsl
+++ shader/fluid/Normal_FS.glsl
@@ -1,17 +1,14 @@
 #version 330
 
 uniform sampler2D depthTex;
-// add more textures
+uniform float texSize;
 
 in vec2 texCoord;
-
 out vec4 color;
+
+float offset = 10.0/texSize;
 
 void main(void)
 {
-	float offset = 1/800;
-    vec4 tangent1 = texture(depthTex, texCoord+vec2(0,offset));
-    vec4 tangent2 = texture(depthTex, texCoord+vec2(offset,0));
-    //color = vec4(tangent1.x);//vec4(cross(tangent1.xyz, tangent2.xyz), 1);
-    color = vec4(texCoord.x, texCoord.y-1, 0, 0);
+	color = texture(depthTex, texCoord);	
 }
