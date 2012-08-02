b853d797c7f51ac21c6b44f56abe244a97461078
bgraf <bgraf@uos.de>
2012-08-02 14:40:50 +0200

    getNormal method in opencl, changed simulation-vs- and fs.

--- shader/simulation_vs.glsl
+++ shader/simulation_vs.glsl
@@ -3,16 +3,17 @@
 uniform mat4 proj;
 uniform mat4 view;
 
-in vec3 positionMC;
+in vec4 positionMC;
 in vec3 normalMC;
 out vec3 coords;
 out vec3 normal;
-
+out vec3 positionFS;
 void main(void)
 {
     gl_Position = proj * view * vec4(positionMC, 1.0);
     coords = positionMC;
     normal = normalMC;
+    positionFS = positionMC.xyz;
 /*
     gl_Position = vec4(positionMC, 0.0, 1.0);
 
