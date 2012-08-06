41bd0b93b2345553c3f5c8593a24c14235f7e584
Faedrivin <info@sebastian-hoeffner.de>
2012-08-06 10:54:26 +0200

    clean up FrameBuffer

--- src/util/FrameBuffer.java
+++ src/util/FrameBuffer.java
@@ -18,66 +18,64 @@ import org.lwjgl.opengl.GL30;
 
 public class FrameBuffer {
 
-private int frameBufferObjectId = -1;
-private List<Texture> textureList = new LinkedList<Texture>();
-private int count;
-private int renderBufferObjectId;
+	private int frameBufferObjectId = -1;
+	private List<Texture> textureList = new LinkedList<Texture>();
+	private int count;
+	private int renderBufferObjectId;
     private int width, height;
 
 
-public void init(boolean depthTest, int width, int height) {
-        this.width = width;
-        this.height = height;
-        count = 0;
-        frameBufferObjectId = GL30.glGenFramebuffers();
-        
-        if(depthTest) {
-            this.bind();
-            renderBufferObjectId = GL30.glGenRenderbuffers();
-            GL30.glBindRenderbuffer(GL30.GL_RENDERBUFFER, renderBufferObjectId);
-            GL30.glRenderbufferStorage(GL30.GL_RENDERBUFFER, GL30.GL_DEPTH_COMPONENT32F, GL.WIDTH, GL.HEIGHT);
-            GL30.glBindRenderbuffer(GL30.GL_RENDERBUFFER, 0);
-            GL30.glFramebufferRenderbuffer(GL30.GL_FRAMEBUFFER, GL30.GL_DEPTH_ATTACHMENT, GL30.GL_RENDERBUFFER, renderBufferObjectId);
-            this.unbind();
-            this.checkForErrors();
-        }
-}
+	public void init(boolean depthTest, int width, int height) {
+	        this.width = width;
+	        this.height = height;
+	        count = 0;
+	        frameBufferObjectId = GL30.glGenFramebuffers();
+	        
+	        if(depthTest) {
+	            this.bind();
+	            renderBufferObjectId = GL30.glGenRenderbuffers();
+	            GL30.glBindRenderbuffer(GL30.GL_RENDERBUFFER, renderBufferObjectId);
+	            GL30.glRenderbufferStorage(GL30.GL_RENDERBUFFER, GL30.GL_DEPTH_COMPONENT32F, GL.WIDTH, GL.HEIGHT);
+	            GL30.glBindRenderbuffer(GL30.GL_RENDERBUFFER, 0);
+	            GL30.glFramebufferRenderbuffer(GL30.GL_FRAMEBUFFER, GL30.GL_DEPTH_ATTACHMENT, GL30.GL_RENDERBUFFER, renderBufferObjectId);
+	            this.unbind();
+	            this.checkForErrors();
+	        }
+	}
 
-public void addTexture(Texture tex, int internalFormat, int format) {
-this.bind();
-bindTexture(tex, GL30.GL_COLOR_ATTACHMENT0 + count, internalFormat, format);
-textureList.add(tex);
-count++;
+	public void addTexture(Texture tex, int internalFormat, int format) {
+		this.bind();
+		bindTexture(tex, GL30.GL_COLOR_ATTACHMENT0 + count, internalFormat, format);
+		textureList.add(tex);
+		count++;
+		this.unbind();
+	}
 
-this.unbind();
-}
-
-public void drawBuffers() {
-     // draw buffers
-     int[] buffersArray = new int[count];
-     for(int i = 0; i < buffersArray.length; i++) {
-     buffersArray[i] = GL30.GL_COLOR_ATTACHMENT0 + i;
-     }
-     IntBuffer buffers = BufferUtils.createIntBuffer(buffersArray.length);
-     buffers.put(buffersArray);
-     buffers.position(0);
-    
-     this.bind();
-     GL20.glDrawBuffers(buffers);
-     this.unbind();
-}
+	public void drawBuffers() {
+	     // draw buffers
+	     int[] buffersArray = new int[count];
+	     for(int i = 0; i < buffersArray.length; i++) {
+	     buffersArray[i] = GL30.GL_COLOR_ATTACHMENT0 + i;
+	     }
+	     IntBuffer buffers = BufferUtils.createIntBuffer(buffersArray.length);
+	     buffers.put(buffersArray);
+	     buffers.position(0);
+	    
+	     this.bind();
+	     GL20.glDrawBuffers(buffers);
+	     this.unbind();
+	}
 
     public Texture getTexture(int i) {
         return textureList.get(i);
     }
     
     public void clearColor() {
-     // set clear color
-     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    	// set clear color
+    	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
     
     public void bind() {
-    	
     	GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, frameBufferObjectId);
         GL11.glViewport(0, 0, this.width, this.height);
 
@@ -91,7 +89,7 @@ public void drawBuffers() {
     
     
     public void unbind() {
-     GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0);
+    	GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0);
     }
     
     public void BindFragDataLocations(ShaderProgram program, String ...fsOutVarNames) {
@@ -100,28 +98,28 @@ public void drawBuffers() {
         }
     }
     
-    /**
-* Binds a texture to the current Frame Buffer Object.
-* @param Texture texture to bind
-* @param attachment Must be GL_COLOR_ATTACHMENT0-15, binding point
-* @param internalFormat GL internal Format
-* @param format GL Format
-*/
+	/**
+	* Binds a texture to the current Frame Buffer Object.
+	* @param Texture texture to bind
+	* @param attachment Must be GL_COLOR_ATTACHMENT0-15, binding point
+	* @param internalFormat GL internal Format
+	* @param format GL Format
+	*/
     private void bindTexture(Texture texture, int attachment, int internalFormat, int format) {
-     // bind texture
-     texture.bind();
-    
-     // add filters
-     GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
-     GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
-    
-     // set texture information
-     GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, internalFormat, this.width, this.height, 0, format, GL11.GL_FLOAT, (FloatBuffer) null);
-    
-     // attach texture to framebuffer
-     GL30.glFramebufferTexture2D(GL30.GL_FRAMEBUFFER, attachment, GL11.GL_TEXTURE_2D, texture.getId(), 0);
-    
-        this.checkForErrors();
+	     // bind texture
+	     texture.bind();
+	    
+	     // add filters
+	     GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+	     GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+	    
+	     // set texture information
+	     GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, internalFormat, this.width, this.height, 0, format, GL11.GL_FLOAT, (FloatBuffer) null);
+	    
+	     // attach texture to framebuffer
+	     GL30.glFramebufferTexture2D(GL30.GL_FRAMEBUFFER, attachment, GL11.GL_TEXTURE_2D, texture.getId(), 0);
+	    
+	     this.checkForErrors();
     }
     
     public void checkForErrors() {
@@ -135,6 +133,7 @@ public void drawBuffers() {
             case GL30.GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER: System.err.println("GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER"); break;
         }
     }
+    
     public void reset(){
     	count=0;
     }
