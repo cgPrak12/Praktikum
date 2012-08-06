882f54f6d3d0e3e257265055a4e56d68a849196f
mibloemer <mibloemer@uos.de>
2012-08-06 13:27:24 +0200

    sysos entfernt

--- src/util/Camera.java
+++ src/util/Camera.java
@@ -13,7 +13,7 @@ public final class Camera {
     private final Vector3f upDir = new Vector3f(0,1,0);
     private final Vector3f sideDir = new Vector3f(1,0,0);
 
-    private final Vector3f camPos = new Vector3f(0,300,0);
+    private final Vector3f camPos = new Vector3f(0,50,0);
 
     private final Matrix4f view = new Matrix4f();
     private final Matrix4f projection = new Matrix4f();
