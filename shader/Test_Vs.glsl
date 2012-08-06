28cbb05d433af5374da01c0e6f43b096e364564f
mibloemer <mibloemer@uos.de>
2012-08-06 14:59:23 +0200

    gfj

--- shader/Test_Vs.glsl
+++ shader/Test_Vs.glsl
@@ -5,10 +5,17 @@ uniform sampler2D elevation;
 
 in vec2 positionMC, vertexTexCoords;
 in vec3 vertexColor;
-out vec2 positionC;
+
+//out vec2 positionMC;
 out vec3 color;
 
 void main(void) {
+
+
+ 
+
+
+
 	vec4 pos = scale * translation *   vec4(positionMC.x, 0 , positionMC.y,1);
     gl_Position = viewProj * model  * vec4(pos.x ,0,pos.z ,1);
 	color = vertexColor;
