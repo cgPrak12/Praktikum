ee20ccc002a997ff11288ef427937caed9021ff1
mibloemer <mibloemer@uos.de>
2012-08-07 16:35:59 +0200

    as

--- shader/Test_Vs.glsl
+++ shader/Test_Vs.glsl
@@ -16,6 +16,7 @@ out vec2 tex;
 void main(void) {
    	vec4 pos = scale * translation *   vec4(positionMC.x, 0, positionMC.y,1);
 	tex = pos.xz / worldSize + 0.5f;
+	
 	float height = heightScale * texture(elevation, tex).x;
     gl_Position = viewProj * model  * vec4(pos.x,height,pos.z,1);
 
--- src/main/TerrainMain.java
+++ src/main/TerrainMain.java
@@ -58,29 +58,22 @@ public class TerrainMain {
             glEnable(GL_DEPTH_TEST);
             glEnable(GL_PRIMITIVE_RESTART);
             glPrimitiveRestartIndex(-1);
-            glCullFace(GL_BACK);
             glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_COLOR);
             program = new ShaderProgram(".\\shader\\Test_Vs.glsl",".\\shader\\Test_Fs.glsl");
             program.use();
 
-            clip = new ClipMap(254, 10, program, cam);
+            clip = new ClipMap(254, 6, program, cam);
             
-            tex = Texture.generateTexture(".\\earth_height.jpg", 1);
+            tex = Texture.generateTexture(".\\Eire_big.png", 1);
             tex.bind();
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+            
             program.setUniform("elevation", tex);
 
-            tex = Texture.generateTexture(".\\earth.jpg", 2);
-            tex.bind();
-            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
-            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
-
-            program.setUniform("coloration", tex);
           
-           
             
             render();
             OpenCL.destroy();
