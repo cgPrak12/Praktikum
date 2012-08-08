91faa56adc54b64266ac4d7055f99dc06730edbe
christoph12345 <cheichler@uos.de>
2012-08-08 09:26:00 +0200

    Bei den Wilden hat jede Familie ihr Obdach, ihre Wohnung, die den Vergleich mit jeder anderen aushält und für gröbere und einfachere Bedürfnisse genügt.

--- shader/Test_Vs.glsl
+++ shader/Test_Vs.glsl
@@ -13,12 +13,8 @@ out vec2 tex;
 
 
 void main(void) {
-   	vec4 pos = scale * translation *   *model *vec4(positionMC.x, 0, positionMC.y,1);
+   	vec4 pos = scale * translation *vec4(positionMC.x, 0, positionMC.y,1);
 	tex = pos.xz / worldSize + 0.5f;
 	float height = heightScale * texture(elevation, tex).x;
-    gl_Position = viewProj  * vec4(pos.x,height,pos.z,1);
-
-	color = vertexColor;
-	//positionWC = texPos;
-	
+    gl_Position = viewProj  * model * vec4(pos.x,height,pos.z,1);
 }    
