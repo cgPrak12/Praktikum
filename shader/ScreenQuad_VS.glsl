e02f2679aed591fa89350c3026a8a319884e6228
Clemens John <clemens-john@gmx.de>
2012-08-13 13:26:14 +0200

    Apply from master

--- shader/ScreenQuad_VS.glsl
+++ shader/ScreenQuad_VS.glsl
@@ -1,23 +1,12 @@
 #version 330
-uniform mat4 viewProj;
-uniform mat4 model;
-uniform mat4 modelIT;
-uniform mat4 scale;
-uniform mat4 translate;
 
-in vec3 positionMC;
-in vec3 normalMC;
-in vec2 vertexTexCoords;
+in vec2 positionMC;
 
-out vec4 positionWC;
-out vec4 normalWC;
-out vec2 fragmentTexCoords;
+out vec2 texCoord;
 
 void main(void)
 {
-    vec4 world = model * vec4(positionMC, 1.0);
-    positionWC = vec4(world.xyz / world.w, 1.0);
-    normalWC = (modelIT * vec4(normalMC, 0.0));
-    fragmentTexCoords = vertexTexCoords;
-    gl_Position = viewProj * translate * scale * world;
+    gl_Position = vec4(positionMC, 0.0, 1.0);
+    texCoord = vec2(0.5, 0.5) + 0.5 * positionMC;
+    // texCoord.y = 1.0 - texCoord.y;
 }
