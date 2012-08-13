e354d3756eef135e433bd4c5a63a6791ac899489
moogle19 <moogle.seidel@gmail.com>
2012-08-13 09:38:25 +0200

    fix

--- shader/ShadowLighting_FS.glsl
+++ shader/ShadowLighting_FS.glsl
@@ -77,7 +77,7 @@ void main(void)
 		
 	//ambient light
 	//float strength = 0.5 + 0.5 * dot(normal, normalize(sunDir));
-	strength = 1;
+	float strength = 1;
 	vec4 ambi = mix(downColor, upColor, strength);
 		
 	//diffuse and specular
